6d12dafdc74dd2e1070e1431f3ba7fa3ff9fc3bb
Kevin Yang <chgc_kevin@hotmail.com>
2018-11-11 16:09:38 +0800

    import ReactiveFormModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
@@ -12,6 +12,7 @@ import { SingupAdvanceComponent } from './singup-advance/singup-advance.componen
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     RouterModule.forRoot([
       { path: 'signup', component: SignupComponent },
