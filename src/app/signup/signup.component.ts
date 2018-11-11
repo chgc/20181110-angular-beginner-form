9c047340e2b5ec8252ff5465b93d45d8ab6dca50
Kevin Yang <chgc_kevin@hotmail.com>
2018-11-11 14:46:02 +0800

    submit form
    取得表單資料

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  @ViewChild('f') form;
   constructor() {}
 
   ngOnInit() {}
+
+  submitForm(f) {
+    // 使用 ViewChild 取得 Form 實體
+    // console.log(this.form);
+
+    // 透過傳值的方式取得 Form實體
+    console.log(f);
+  }
 }
